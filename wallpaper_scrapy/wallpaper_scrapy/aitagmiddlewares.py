1e4ab637502d41d70dc900e8208a83fb89557648
alisonlai <mr.lai.who@gmail.com>
2024-02-22 19:19:13 +0800

    feat: 🎸 ai 分析成 tag

--- wallpaper_scrapy/wallpaper_scrapy/aitagmiddlewares.py
+++ wallpaper_scrapy/wallpaper_scrapy/aitagmiddlewares.py
@@ -1,12 +1,9 @@
-# 导入必要的库
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.translation.text import TextTranslationClient, TranslatorCredential
-
-
+import deepl
 import os
 
 # 设置 Azure 计算机视觉服务的密钥和终结点
@@ -23,15 +20,10 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 text_analytics_client = TextAnalyticsClient(text_analytics_endpoint, AzureKeyCredential(text_analytics_key))
 
 # 替换为您自己的Azure订阅密钥和终结点
-translator_key = '3fc1274301664a6ab753d766ae91c95f'
-translator_endpoint = 'https://api.cognitive.microsofttranslator.com/'
-
-# 创建认证对象
-credential = AzureKeyCredential(translator_key)
+translator_key = '5387c78d-cf8e-0421-2989-7282a5a03c84:fx'
 
 # 创建Translator Text客户端
-translation_client = TextTranslationClient(credential = TranslatorCredential(translator_key, translator_endpoint));
-
+translation_client = translator = deepl.Translator(translator_key)
 
 
 # 加载图像
@@ -48,36 +40,31 @@ image_tags = [tag.name for tag in image_analysis.tags]
 # 这里只是一个示例，实际情况可能更复杂
 
 text_from_image = """
-The United States of America has had 46 commanders in chief, and today, the third Monday of February, we commemorate their legacies. Initially established to honor George Washington's birthday, Presidents Day has evolved into a celebration of all presidents. Revered among them is Abraham Lincoln, a towering figure in American history, who guided the country through the Civil War and abolished slavery in America. The Lincoln Memorial, seen in today's image, overlooks the Reflecting Pool at the west end of the National Mall in Washington, DC. Designed in a neoclassical style, the memorial is a poignant destination for those seeking to connect with American history during Presidents Day, and beyond.
+美利坚合众国曾有 46 位总司令，今天，二月的第三个星期一，我们纪念他们的丰功伟绩。总统日最初是为了纪念乔治-华盛顿（George Washington）的生日而设立的，如今已发展成为庆祝所有总统的节日。亚伯拉罕-林肯是美国历史上备受尊敬的人物，他领导美国经历了南北战争，并废除了美国的奴隶制。林肯纪念堂位于华盛顿特区国家广场西端，俯瞰倒影池。林肯纪念堂采用新古典主义风格设计，是美国总统日期间及之后人们追寻美国历史的一个重要景点。
 """
-
-
-# 指定目标语言
-target_language = 'zh-Hans'
+#The United States of America has had 46 commanders in chief, and today, the third Monday of February, we commemorate their legacies. Initially established to honor George Washington's birthday, Presidents Day has evolved into a celebration of all presidents. Revered among them is Abraham Lincoln, a towering figure in American history, who guided the country through the Civil War and abolished slavery in America. The Lincoln Memorial, seen in today's image, overlooks the Reflecting Pool at the west end of the National Mall in Washington, DC. Designed in a neoclassical style, the memorial is a poignant destination for those seeking to connect with American history during Presidents Day, and beyond.
+#"""
 
 # 调用翻译方法进行文本翻译
-result = translation_client.transliterate(text_from_image, target_language)
+target_languages = "ZH"
+
+response = translation_client.translate_text(text_from_image, target_lang=target_languages)
+
+documents = [
+    ",".join(image_tags),
+    response.text
+]
+
+response = text_analytics_client.recognize_entities(documents=documents)
+sorted_response = sorted(response, key=lambda x: x.entities[0].confidence_score, reverse=True)
 
 
-# 提取翻译后的文本
-translated_text = result.translations[0].text
+tags = set()
+# 遍历排序后的结果
+for item in sorted_response:
+    for entity in item.entities:
+        print(entity.text, entity.category, entity.confidence_score)
+        if entity.confidence_score > 0.9:
+            tags.add(entity.text)    
 
-print(f"Translated Text: {translated_text}")
-
-# 将图像标签和文字信息结合在一起
-combined_text = ",".join(image_tags) + translated_text
-
-# 提取标签
-response = text_analytics_client.extract_key_phrases(documents=[combined_text], language="zh")
-
-for document in response:
-    if not document.is_error:
-        print("Document Key Phrases:")
-        for phrase in document.key_phrases:
-            print("\t", phrase)
-    else:
-        print("Error: ", document.id, document.error)
-
-
-
-# 在这里，您可以根据需要进一步处理和利用生成的标签信息
+print(tags)
